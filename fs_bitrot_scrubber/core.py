b08b8f4f1ee20baf8c76a9d6e8dacd00908861c9
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-06 00:46:30 +0600

    Implemented basic check functionality

--- fs_bitrot_scrubber/core.py
+++ fs_bitrot_scrubber/core.py
@@ -1,16 +1,158 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import itertools as it, operator as op, functools as ft
-import os, sys
+from os.path import realpath, join, isdir, dirname, basename, exists
+from time import time, sleep
+import os, sys, re, hashlib, stat, types, logging
 
 
-# try: from fs_bitrot_scrubber import db
-# except ImportError:
-# 	# Make sure it works from a checkout
-# 	if isdir(join(dirname(__file__), 'fs_bitrot_scrubber'))\
-# 			and exists(join(dirname(__file__), 'setup.py')):
-# 		sys.path.insert(0, dirname(__file__))
-# 	from fs_bitrot_scrubber import db
+try: from fs_bitrot_scrubber.db import MetaDB
+except ImportError:
+	# Make sure it works from a checkout
+	if isdir(join(dirname(__file__), 'fs_bitrot_scrubber'))\
+			and exists(join(dirname(__file__), 'setup.py')):
+		sys.path.insert(0, dirname(__file__))
+	from fs_bitrot_scrubber.db import MetaDB
+
+
+is_str = lambda obj,s=types.StringTypes: isinstance(obj, s)
+
+_re_type = type(re.compile(''))
+
+def check_filters(path, filters, default=True):
+	path = '/' + path
+	for rule in filters:
+		try: x, pat = rule
+		except (TypeError, ValueError): x, pat = False, rule
+		if not isinstance(pat, _re_type): pat = re.compile(pat)
+		if pat.search(path): return x
+	return default
+
+def token_bucket(metric, spec):
+	try:
+		try: interval, burst = spec.rsplit(':', 1)
+		except (ValueError, AttributeError): interval, burst = spec, 1.0
+		else: burst = float(burst)
+		if is_str(interval):
+			try: a, b = interval.split('/', 1)
+			except ValueError: interval = float(interval)
+			else: interval = op.truediv(*it.imap(float, [a, b]))
+		if min(interval, burst) < 0: raise ValueError()
+	except:
+		raise ValueError('Invalid format for rate limit (metric: {}): {!r}'.format(metric, spec))
+
+	tokens, rate, ts_sync = burst, interval**-1, time()
+	val = yield
+	while True:
+		ts = time()
+		ts_sync, tokens = ts, min(burst, tokens + (ts - ts_sync) * rate)
+		val, tokens = (None, tokens - val)\
+			if tokens >= val else ((val - tokens) / rate, tokens - val)
+		val = yield val
+
+
+
+def file_list(paths, xdev=True, path_filter=list()):
+	_check_filters = ft.partial(check_filters, filters=path_filter)
+	paths = set(it.imap(realpath, paths))
+
+	while paths:
+		path_base = paths.pop()
+		path_base_dev = os.stat(path_base).st_dev
+
+		for p, dirs, files in os.walk(path_base, topdown=True):
+			if xdev and p not in paths and os.stat(p).st_dev != path_base_dev: continue
+			paths.discard(p)
+
+			i_off = 0
+			for i, name in list(enumerate(dirs)):
+				path = join(p, name)
+				# Filtered-out dirs won't be descended into
+				if not _check_filters(path + '/'):
+					del dirs[i - i_off]
+					i_off += 1 # original list just became shorter
+
+			for name in files:
+				path = join(p, name)
+				if not _check_filters(path): continue
+				fstat = os.lstat(path)
+				if not stat.S_ISREG(fstat.st_mode): continue
+				yield path, fstat
+
+
+def scrub( paths, meta_db,
+		xdev=True, path_filter=list(),
+		skip_for=3 * 3600, bs=4 * 2**20, rate_limits=None ):
+	log = logging.getLogger('scrubber')
+	log.debug('Scrub generation number: {}'.format(meta_db.generation))
+
+	scan_limit = getattr(rate_limits, 'scan', None)
+	read_limit = getattr(rate_limits, 'read', None)
+	delay_ts = 0 # deadline for the next limit
+
+	file_node = None # currently scrubbed (checksummed) file
+
+	for path, fstat in file_list(paths, xdev=xdev, path_filter=path_filter):
+		# Bumps generaton number on path as well, to facilitate cleanup
+		meta_db.metadata_check( path,
+			size=fstat.st_size, mtime=fstat.st_mtime, ctime=fstat.st_ctime )
+
+		# Scan always comes first, unless hits the limit
+		if not scan_limit: continue
+		delay = scan_limit.send(1)
+		if not delay: continue
+		ts = time()
+		if ts + delay < delay_ts: continue # reads are still banned
+		delay_ts = ts + delay
+
+		while True:
+			if ts >= delay_ts: break
+
+			if not file_node: # pick next node
+				# TODO: check mtime after hashing to see if file changed since then
+				file_node = meta_db.get_file_to_scrub(skip_for=skip_for)
+			if not file_node: # nothing left/yet in this generation
+				delay = max(0, delay_ts - ts)
+				if delay:
+					log.debug('Rate-limiting delay (scan): {:.1f}s'.format(delay))
+					sleep(delay)
+				break
+
+			bs_read = file_node.read(bs)
+			if not bs_read: # done with this one
+				file_node.close()
+				file_node = None
+			ts = time()
+
+			if read_limit: # min delay
+				delay = read_limit.send(bs_read)
+				if delay:
+					if ts + delay >= delay_ts:
+						delay_ts = ts + delay
+						break # scan comes next
+					else:
+						log.debug('Rate-limiting delay (read): {:.1f}s'.format(delay))
+						sleep(delay)
+						ts = time()
+
+	# Drop all meta-nodes for files with old generation
+	meta_db.metadata_clean()
+
+	# Check the rest of non-clean files in this gen
+	while True:
+		if not file_node: file_node = meta_db.get_file_to_scrub(skip_for=skip_for)
+		if not file_node: break
+		bs_read = file_node.read(bs)
+		if not bs_read:
+			file_node.close()
+			file_node = None
+		if read_limit:
+			delay = read_limit.send(bs_read)
+			if delay:
+				log.debug('Rate-limiting delay (read): {:.1f}s'.format(delay))
+				sleep(delay)
+
 
 
 def main(argv=None):
@@ -34,12 +176,35 @@ def main(argv=None):
 	for k in optz.config: cfg.update_yaml(k)
 
 	## Logging
-	import logging
 	logging.basicConfig(
 		level=logging.WARNING if not optz.debug else logging.DEBUG )
 	log = logging.getLogger()
 
-	raise NotImplementedError()
+	## Options processing
+	try: cfg.operation.checksum = getattr(hashlib, cfg.operation.checksum)
+	except AttributeError: cfg.operation.checksum = hashlib.new(cfg.operation.checksum)
+	if is_str(cfg.storage.path): cfg.storage.path = [cfg.storage.path]
+	_filter_actions = {'+': True, '-': False}
+	cfg.storage.filter = list(
+		(_filter_actions[pat[0]], re.compile(pat[1:]))
+		for pat in (cfg.storage.filter or list()) )
+	for metric, spec in cfg.operation.rate_limit.viewitems():
+		if not spec: continue
+		spec = token_bucket(metric, spec)
+		next(spec)
+		cfg.operation.rate_limit[metric] = spec
+	if not cfg.storage.metadata.db_parity:
+		cfg.storage.metadata.db_parity = cfg.storage.metadata.db + '.check'
+	skip_for = cfg.operation.skip_for_hours * 3600
+	cfg.operation.read_block = int(cfg.operation.read_block)
+
+	## Actual work
+	with MetaDB( cfg.storage.metadata.db,
+			cfg.storage.metadata.db_parity, cfg.operation.checksum,
+			log_queries=cfg.logging.sql_queries ) as meta_db:
+		scrub( cfg.storage.path, meta_db,
+			xdev=cfg.storage.xdev, path_filter=cfg.storage.filter,
+			skip_for=skip_for, bs=cfg.operation.read_block, rate_limits=cfg.operation.rate_limit )
 
 	log.debug('Done')
 
---
+++ fs_bitrot_scrubber/db.py
@@ -0,0 +1,203 @@
+#-*- coding: utf-8 -*-
+
+import itertools as it, operator as op, functools as ft
+from contextlib import contextmanager, closing
+from time import time
+from os.path import exists
+import os, sys, sqlite3, logging, hashlib
+
+
+class FileNode(object):
+
+	def __init__(self, query_func, log, row, checksum):
+		self.q, self.log, self.meta = query_func, log, row
+		self.src = open(row['path'])
+		self.log.debug('Checking file: {}'.format(row['path']))
+		self.src_meta, self.src_checksum = self.stat(), checksum()
+
+	def stat(self):
+		# ctime change is also important here,
+		#  as it may indicate changes with reverted mtime,
+		#  which will produce false-positive otherwise
+		return op.attrgetter('st_size', 'st_ctime', 'st_mtime')(os.fstat(self.src.fileno()))
+
+	def read(self, bs=2 * 2**20):
+		chunk = self.src.read(bs)
+		if self.stat() != self.src_meta:
+			# Bail out if file changes while it's being hashed
+			self.q( 'UPDATE files SET dirty = 1,'
+				' last_skip = ? WHERE path = ?', (time(), self.meta['path']) )
+			return 0
+		if chunk: self.src_checksum.update(chunk)
+		else:
+			digest = self.src_checksum.digest()
+			size, ctime, mtime = self.src_meta
+			if self.meta['checksum'] != digest: # either new hash or changes
+				if self.meta['checksum'] is not None: # can still be intentional change w/ reverted mtime
+					if max(abs(self.meta['ctime'] - ctime), abs(self.meta['mtime'] - mtime)) >= 1:
+						# TODO: flag to log these as warnings maybe?
+						self.log.info( 'Detected change in'
+							' file contents and ctime: {}'.format(self.meta['path']) )
+					else: # bitrot!!!
+						self.log.error('Detected unmarked changes: {}'.format(self.meta['path']))
+			# Update with last-seen metadata,
+			#  regardless of what was set in metadata_check()
+			self.q( 'UPDATE files SET dirty = 0, clean = 1,'
+					' size = ?, mtime = ?, ctime = ?, checksum = ?, last_scrub = ?,'
+					' last_skip = NULL WHERE path = ?',
+				(size, mtime, ctime, digest, time(), self.meta['path']) )
+		return len(chunk)
+
+	def close(self):
+		self.src.close()
+		self.src = self.src_meta = self.src_checksum = None
+
+
+class MetaDB(object):
+
+	# clean - file was checked in this generation
+	# dirty - mtime/size was updated in this generation
+	# checksum - hash (binary)
+	# last_scrub - last time "clean" was set to true
+	# last_skip - last time failed to checksum due to rapid changes
+	_db_init = '''
+		CREATE TABLE IF NOT EXISTS files (
+			path BLOB PRIMARY KEY ON CONFLICT REPLACE NOT NULL,
+			generation INT NOT NULL,
+			size INT NOT NULL,
+			mtime REAL NOT NULL,
+			ctime REAL NOT NULL,
+			clean BOOLEAN NOT NULL,
+			dirty BOOLEAN NOT NULL,
+			checksum BLOB NULL,
+			last_scrub REAL NULL,
+			last_skip REAL NULL
+		);
+		CREATE INDEX IF NOT EXISTS files_checksum
+			ON files (generation, checksum, last_skip, last_scrub);
+		CREATE INDEX IF NOT EXISTS files_clean
+			ON files (generation, clean, last_skip, last_scrub);
+		CREATE INDEX IF NOT EXISTS files_dirty
+			ON files (generation, dirty, last_skip, last_scrub);
+		CREATE INDEX IF NOT EXISTS files_gen
+			ON files (generation);
+
+		CREATE TABLE IF NOT EXISTS meta (
+			var TEXT PRIMARY KEY ON CONFLICT REPLACE NOT NULL,
+			val TEXT NOT NULL
+		);
+	'''
+
+	_db_migrations = []
+
+	_db = None
+
+
+	def __init__( self, path, path_check=None,
+			checksum=None, log=None, log_queries=False ):
+		self._log = logging.getLogger('scrubber.MetaDB') if not log else log
+		self._log_sql = log_queries
+		self._checksum = hashlib.sha256 if not checksum else checksum
+		self._db_path, self._db_parity = path, path_check
+		self._init_db()
+		self.generation = self.get_generation()
+
+	@contextmanager
+	def _cursor(self, query, params=tuple(), **kwz):
+		if self._log_sql:
+			self._log.debug('Query: {!r}, data: {!r}'.format(query, params))
+		with self._db as db:
+			with closing(db.execute(query, params, **kwz)) as c:
+				yield c
+
+	def _query(self, *query_argz, **query_kwz):
+		with self._cursor(*query_argz, **query_kwz): pass
+
+	def _parity_check(self):
+		# TODO: use zfec or something similar here
+		if not self._db_parity or not exists(self._db_parity): return
+		digest = hashlib.sha256()
+		with open(self._db_path) as db:
+			for chunk in iter(ft.partial(db.read, 2**20), ''): digest.update(chunk)
+		assert open(self._db_parity).read().strip() == digest.hexdigest(), 'DB check failed'
+
+	def _parity_write(self):
+		# TODO: use zfec or something similar here
+		if not self._db_parity: return
+		digest = hashlib.sha256()
+		with open(self._db_path) as db:
+			for chunk in iter(ft.partial(db.read, 2**20), ''): digest.update(chunk)
+		open(self._db_parity, 'w').write(digest.hexdigest())
+
+	def _init_db(self):
+		self._parity_check()
+		self._db = sqlite3.connect(self._db_path)
+		self._db.row_factory, self._db.text_factory = sqlite3.Row, str
+		with self._db as db: db.executescript(self._db_init)
+		with self._cursor("SELECT val FROM meta WHERE var = 'schema_version' LIMIT 1") as c:
+			row = c.fetchone()
+			schema_ver = int(row['val']) if row else 1
+		for schema_ver, query in enumerate(
+			self._db_migrations[schema_ver-1:], schema_ver ): db.executescript(query)
+		self._query( 'INSERT INTO meta (var, val)'
+			" VALUES ('schema_version', '{}')".format(schema_ver + 1) )
+
+	def close(self):
+		if self._db:
+			self._db.close()
+			self._db = None
+			if exists(self._db_path):
+				self._parity_write()
+
+	def __enter__(self): return self
+	def __exit__(self, *err): self.close()
+	def __del__(self): self.close()
+
+
+	def get_generation(self, new=True):
+		with self._cursor('SELECT generation'
+				' FROM files ORDER BY generation DESC LIMIT 1') as c:
+			row = c.fetchone()
+		gen = row['generation'] if row else 0
+		if new: gen += 1
+		return gen
+
+	def metadata_check(self, path, size, mtime, ctime):
+		with self._cursor('SELECT * FROM files WHERE path = ? LIMIT 1', (path,)) as c:
+			row = c.fetchone()
+		if not row:
+			self._query( 'INSERT INTO files (path, generation,'
+					' size, mtime, ctime, clean, dirty) VALUES (?, ?, ?, ?, ?, 0, 0)',
+				(path, self.generation, size, mtime, ctime) )
+			return True
+		dirty = row['dirty']
+		if not dirty and not (abs(row['mtime'] - mtime) <= 1 and row['size'] == size): dirty = True
+		else: ctime = row['ctime'] # so it won't be set to a new value
+		self._query( 'UPDATE files SET generation = ?, ctime = ?,'
+			' clean = 0, dirty = ? WHERE path = ?', (self.generation, ctime, dirty, path) )
+		return dirty
+
+	def metadata_clean(self):
+		self._query('DELETE FROM files WHERE generation < ?', (self.generation,))
+
+	def get_file_to_scrub(self, skip_for=3 * 3600, skip_until=0):
+		query_base = 'SELECT * FROM files WHERE generation = ?'\
+			' AND (last_skip IS NULL OR last_skip < ?) {} ORDER BY last_scrub LIMIT 1'
+		query_params = [self.generation, skip_until]
+		# First try to hash not-yet-seen files
+		with self._cursor(query_base.format('AND checksum IS NULL'), query_params) as c:
+			row = c.fetchone()
+		if not row:
+			# Then dirty (changed) files
+			with self._cursor(query_base.format('AND dirty = 1'), query_params) as c:
+				row = c.fetchone()
+		if not row:
+			# Then just not-yet-checked for this generation
+			with self._cursor(query_base.format('AND clean = 0'), query_params) as c:
+				row = c.fetchone()
+		if not row and skip_until == 0:
+			# Then try to find a path that was skipped a while ("skip_for") ago
+			skip_until = time() - skip_for
+			if skip_until != 0: return self.get_file_to_scrub(skip_until=skip_until)
+		if not row: return # nothing more/yet to check
+		return FileNode(self._query, self._log, row, checksum=self._checksum)
