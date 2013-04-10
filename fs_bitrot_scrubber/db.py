f3b34f3b9f356c405bf7196bab34ac4fe771765f
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-10 14:43:13 +0600

    Handle scanned paths vanishing in a graceful manner

--- fs_bitrot_scrubber/db.py
+++ fs_bitrot_scrubber/db.py
@@ -12,9 +12,8 @@ from fs_bitrot_scrubber import force_unicode
 
 class FileNode(object):
 
-	def __init__(self, query_func, log, row, checksum):
-		self.q, self.log, self.meta = query_func, log, row
-		self.src = open(row['path'])
+	def __init__(self, query_func, log, src, row, checksum):
+		self.q, self.log, self.meta, self.src = query_func, log, row, src
 		self.log.debug(force_unicode('Checking file: {}'.format(row['path'])))
 		self.src_meta, self.src_checksum = self.stat(), checksum()
 
@@ -186,27 +185,35 @@ class MetaDB(object):
 		self._query('DELETE FROM files WHERE generation < ?', (self.generation,))
 
 	def get_file_to_scrub(self, skip_for=3 * 3600, skip_until=0):
-		query_base = 'SELECT * FROM files WHERE generation = ?'\
-			' AND (last_skip IS NULL OR last_skip < ?) {} ORDER BY last_scrub LIMIT 1'
-		query_params = [self.generation, skip_until]
-		# First try to hash not-yet-seen files
-		with self._cursor(query_base.format('AND checksum IS NULL'), query_params) as c:
-			row = c.fetchone()
-		if not row:
-			# Then dirty (changed) files
-			with self._cursor(query_base.format('AND dirty = 1'), query_params) as c:
+		while True:
+			query_base = 'SELECT * FROM files WHERE generation = ?'\
+				' AND (last_skip IS NULL OR last_skip < ?) {} ORDER BY last_scrub LIMIT 1'
+			query_params = [self.generation, skip_until]
+			# First try to hash not-yet-seen files
+			with self._cursor(query_base.format('AND checksum IS NULL'), query_params) as c:
 				row = c.fetchone()
-		if not row:
-			# Then just not-yet-checked for this generation
-			with self._cursor(query_base.format('AND clean = 0'), query_params) as c:
-				row = c.fetchone()
-		if not row and skip_until == 0:
-			# Then try to find a path that was skipped a while ("skip_for") ago
-			skip_until = time() - skip_for
-			if skip_until != 0: return self.get_file_to_scrub(skip_until=skip_until)
-		if not row: return # nothing more/yet to check
-		return FileNode(self._query, self._log, row, checksum=self._checksum)
+			if not row:
+				# Then dirty (changed) files
+				with self._cursor(query_base.format('AND dirty = 1'), query_params) as c:
+					row = c.fetchone()
+			if not row:
+				# Then just not-yet-checked for this generation
+				with self._cursor(query_base.format('AND clean = 0'), query_params) as c:
+					row = c.fetchone()
+			if not row and skip_until == 0:
+				# Then try to find a path that was skipped a while ("skip_for") ago
+				skip_until = time() - skip_for
+				if skip_until != 0: return self.get_file_to_scrub(skip_until=skip_until)
+			if not row: return # nothing more/yet to check
+			try: src = open(row['path'])
+			except (IOError, OSError):
+				self._log.debug('Failed to open scanned path, skipping it: {!r}'.format(row['path']))
+				self.drop_file(row['path'])
+				continue
+			return FileNode(self._query, self._log, src, row, checksum=self._checksum)
 
+	def drop_file(self, path):
+		self._query('DELETE FROM files WHERE generation = ? AND path = ?', (self.generation, path))
 
 	def list_paths(self):
 		with self._cursor('SELECT * FROM files') as c:
